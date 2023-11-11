d05514cfe60a9d0ce78896daeab02bd3e94427e6
sebaacr <150304616+sebaacr@users.noreply.github.com>
2023-11-11 12:51:32 -0300

    aqui pase a otra pagina del index

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-function crearCirculo(area) {
-    const coords = area.getAttribute("coords").split(",");
-    const left = parseInt(coords[0]);
-    const top = parseInt(coords[1]);
-    const radius = parseInt(coords[2]);
+let contadorFiguras = 0;
+let areasClicadas = [];
 
-    const circle = document.createElement("div");
-    circle.className = "circle";
-    circle.style.left = left + "px";
-    circle.style.top = top + "px";
-    circle.style.width = radius * 2 + "px";
-    circle.style.height = radius * 2 + "px";
+function crearCirculo(area, figuraTexto) {
+    const coords = area.getAttribute("coords");
 
-    document.querySelector(".recuadro").appendChild(circle);
+    // Verificar si el área ya ha sido clicada
+    if (!areasClicadas.includes(coords)) {
+        // Incrementar el contador de figuras
+        contadorFiguras++;
+
+        // Agregar el área a la lista de áreas clicadas
+        areasClicadas.push(coords);
+
+        // Obtener o crear el contenedor lateral izquierdo
+        let lateralIzquierdo = document.querySelector(".lateralDerecho");
+
+        if (!lateralIzquierdo) {
+            // Si el lateral izquierdo no existe, créalo
+            lateralIzquierdo = document.createElement("div");
+            lateralIzquierdo.className = "lateralDerecho";
+            document.body.appendChild(lateralIzquierdo);
+        }
+
+        // Crear un nuevo recuadro para mostrar el texto
+        const figuraTextoElemento = document.createElement("div");
+        figuraTextoElemento.className = "textoRecuadro";
+        figuraTextoElemento.innerHTML = figuraTexto || "Figura Desconocida"; // Usar el texto proporcionado o un valor predeterminado
+
+        // Agregar el nuevo recuadro al lateral izquierdo
+        lateralIzquierdo.appendChild(figuraTextoElemento);
+    }
 }
