a13a88a47cf7a60fd749d71aaa259c7ee5174d8f
Burgos100 <lucasxddd13b@gmail.com>
2023-11-13 20:25:31 -0300

    1er avance

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function crearCirculo(area) {
     const top = parseInt(coords[1]);
     const radius = parseInt(coords[2]);
 
+<<<<<<< Updated upstream
     const circle = document.createElement("div");
     circle.className = "circle";
     circle.style.left = left + "px";
@@ -13,3 +14,35 @@ function crearCirculo(area) {
 
     document.querySelector(".recuadro").appendChild(circle);
 }
+=======
+function crearCirculo(area, figuraTexto) {
+    const coords = area.getAttribute("coords");
+
+    // Verificar si el área ya ha sido clicada
+    if (!areasClicadas.includes(coords)) {
+        // Incrementar el contador de figuras
+        contadorFiguras++;
+
+        // Agregar el área a la lista de áreas clicadas
+        areasClicadas.push(coords);
+
+        // Obtener o crear el contenedor lateral izquierdo
+        let lateralIzquierdo = document.querySelector(".lateralDerecho");
+
+        if (!lateralIzquierdo) {
+            // Si el lateral izquierdo no existe, créalo
+            lateralIzquierdo = document.createElement("div");
+            lateralIzquierdo.className = "lateralDerecho";
+            document.body.appendChild(lateralIzquierdo);
+        }
+
+        // Crear un nuevo recuadro para mostrar el texto
+        const figuraTextoElemento = document.createElement("div");
+        figuraTextoElemento.className = "textoRecuadro";
+        figuraTextoElemento.innerHTML = figuraTexto || "Figura Desconocida"; // Usar el texto proporcionado o un valor predeterminado
+
+        // Agregar el nuevo recuadro al lateral izquierdo
+        lateralIzquierdo.appendChild(figuraTextoElemento);
+    }
+}
+>>>>>>> Stashed changes
